5962830105ea1bafa40352e89ecca8eff822ad65
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-01-02 11:23:24 -0700

    Create: replace for loop using while loop

--- javascript-info/even-num-2-10.js
+++ javascript-info/even-num-2-10.js
@@ -5,3 +5,15 @@ for (let i = 1; i < 11; i++) {
     alert( i );
   }
 }
+
+// Rewrite the code changing the for loop to while without altering its behavior (the output should stay same).
+
+for (let i = 0; i < 3; i++) {
+  alert( `number ${i}!` );
+}
+
+let i = 0;
+do {
+  alert( i );
+  i++;
+} while(i < 3);
