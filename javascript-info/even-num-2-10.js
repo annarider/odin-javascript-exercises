00917b0018aa9cfbdcd6074e7f073847939a5733
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-01-02 11:43:34 -0700

    Update: fix infinite loop bug
    Checked answer and improved functionality using
    do-while loop, changing prompt to number rather
    than using string input, and correctly checking
    for nulls.

--- javascript-info/even-num-2-10.js
+++ javascript-info/even-num-2-10.js
@@ -21,7 +21,7 @@ do {
 
 // Write a loop which prompts for a number greater than 100. If the visitor enters another number – ask them to input again.
 
-let num = Number(prompt("Enter a number greater than 100?", "0"));
-while (num < 100 && num != NaN) {
-  prompt("Enter a number greater than 100?", "0");
-}
+let num;
+do {
+  num = prompt("Enter a number greater than 100?", 0);
+} while(num <= 100 && num);
