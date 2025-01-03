575d5c8df969cfead2beb245652daf325a6a0252
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-01-03 13:52:55 -0700

    Update: loop args and remove from original array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
 const removeFromArray = function(array, ...theStrings) {
+  const removeStrings = Array.from(theStrings);
+
+  for (let i = 0; i < removeStrings.length; i++) {
+
+    const index = array.indexOf(i);
+    if (index > -1) { // only splice array when item is found
+      array.splice(index);
+  } 
+
+  return array; 
+}
   
 };
 
