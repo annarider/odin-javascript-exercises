70865244b1cfcff5dfddae1377008b730bc441e1
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-01-03 14:07:06 -0700

    Update: loop through difference and add to total

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(startInt, endInt) {
+  let total = 0;
 
+  for (i = startInt; i <= endInt; i++) {
+    total += startInt;
+    console.log(total);
+  }
+
+  return total;
 };
 
 // Do not edit below this line
