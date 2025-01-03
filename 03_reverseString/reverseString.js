e5d000a5460b113135103694fe05e09ea1d48f83
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-01-03 13:34:37 -0700

    Update: array has functions to add letter at start

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(phrase) {
+  let result = [];
 
+  for ( let i = 0; i < phrase.length; i++ ) {
+    result.unshift( charAt(i) );
+  }
+  
+  return result.join('');
 };
 
 // Do not edit below this line
