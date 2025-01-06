77a03cd753e7e8963886794285b2a47a7f2daf43
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-01-06 10:58:04 -0700

    Fix: Corrected logical bug on isNaN
    If a value is a number, isNaN() returns false
    So !isNaN() returns true for numbers. The first
    if statement checked for non-numbers, so the
    logic was reversed.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -2,29 +2,29 @@ const sumAll = function(startInt, endInt) {
   let total = 0;
 
   // Test for integer
-  if( parseInt(startInt) === startInt && parseInt(endInt) === endInt &&
+  if( parseInt(startInt) !== startInt || parseInt(endInt) !== endInt ||
       // test for positive 
-      startInt > 0 && endInt > 0 &&
+      startInt < 0 || endInt < 0 ||
       // test for numbers
-      !isNaN(startInt) && !isNaN(endInt)
-    ) {
- 
-      if (startInt < endInt) {
-        
-        while (startInt <= endInt) {
-          total += startInt;
-          startInt++;
-        }
-        
-      } else if (startInt > endInt) {
-        
-        while (startInt >= endInt) {
-          total += startInt;
-          startInt--;
-          
-        }
-      }
+      isNaN(startInt) || isNaN(endInt)) {
+
+    return 'ERROR';
+  } else if (startInt < endInt) {
+    
+    while (startInt <= endInt) {
+      total += startInt;
+      startInt++;
     }
+        
+  } else if (startInt > endInt) {
+        
+    while (startInt >= endInt) {
+      total += startInt;
+      startInt--;
+      
+    }
+      }
+
   return total;
 };
 
