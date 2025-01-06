1acdd126fe7e4b89c1a84e5ec8c654828c8c358b
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-01-06 11:36:05 -0700

    Update: Add C to F conversion following same logic

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,16 @@
-const convertToCelsius = function(temperature) {
-  // Formula is (x − 32) × 5/9 ⁠
-  return ( temperature - 32 ) * 5/9;
+const convertToCelsius = function( temperature ) {
+  // Formula is (F − 32) × 5/9 ⁠
+  celsiusTemp = ( temperature - 32 ) * 5/9;
+
+  // round to 1 decimal
+  return Math.round( celsiusTemp * 10 ) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function( temperature ) {
+  // Formula is (C * 9/5) + 32
+  fahrenTemp = ( temperature * 9/5 ) + 32;
+
+  return Math.round( fahrenTemp * 10 ) / 10;
 };
 
 // Do not edit below this line
