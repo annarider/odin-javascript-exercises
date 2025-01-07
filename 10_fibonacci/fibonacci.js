ffcbc98ee4bff5c1be21ab05441b05b74446b22b
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-01-07 10:52:37 -0700

    Add: fibonacci logic including handling NaN

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+  let number = Number(n);
+  if (number < 0 ) return 'OOPS';
+  // Handle exception of fibonacci number of 0
+  if (number === 0) return 0;
+  const fibonacciSequence = [1, 1];
+  for (i = 2; i < number; i++) {
+    // formula: (i-1) + (i-2)
+    fibonacciSequence.push(
+      fibonacciSequence[i - 1] + fibonacciSequence[i - 2]
+    )  
+  }
+  return fibonacciSequence[number - 1];
 };
 
 // Do not edit below this line
