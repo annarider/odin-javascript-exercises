804497cdd649b791613ce8468b68172bf5e10089
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-01-07 10:18:08 -0700

    Refactor: Remove debugging output

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,7 +1,6 @@
 const palindromes = function (string) {
   let standardizedString = string.toLowerCase().replace(/[.,/#!$%^&*;:{}=\-_`~() ]/g, "");
   let reversedString = standardizedString.split("").reverse().join("");
-  console.log(reversedString);
   return reversedString === standardizedString ? true : false;
 };
 
