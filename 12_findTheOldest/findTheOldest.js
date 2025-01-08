04a54a99ff09aaaba3b03c8ae6a15caf1117706d
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-01-08 10:57:18 -0700

    Add: Logic to calculate age & track oldest person

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  let oldestPerson = {};
+  let oldestAge = '0';
+  for (i = 0; i < people.length; i++) {
+    let person = people[i];
+    console.log(typeof person);
+    let age = person.yearOfDeath - person.yearOfBirth;
+    if (age > oldestAge) {
+      oldestAge = age;
+      oldestPerson = person;
+    }
+  }
+  console.log(oldestPerson);
+  console.log(`${oldestPerson}: ${oldestAge}`);
+  return oldestPerson;
 };
 
 // Do not edit below this line
