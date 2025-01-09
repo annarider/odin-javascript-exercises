38cb4a9981b955147a64276bff3a345a38627534
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-01-09 13:13:16 -0700

    Add: Buttons event listener functions to test

--- dom-manipulation/js-exercise.js
+++ dom-manipulation/js-exercise.js
@@ -33,12 +33,35 @@ aside.append(p);
 
 container.append(aside);
 
-// the JavaScript file
+function alertFunction() {
+  alert("YAY! YOU DID IT!");
+}
 const btn = document.querySelector("#btn");
-btn.onclick = () => alert("Hello World");
 
-// the JavaScript file
+btn.addEventListener("click", alertFunction);
+btn.addEventListener("click", function (e) {
+  console.log(e);
+});
+btn.addEventListener("click", function (e) {
+  console.log(e.target);
+});
+btn.addEventListener("click", function (e) {
+  e.target.style.background = "blue";
+});
+
+
 const btn2 = document.querySelector("#btn2");
 btn2.addEventListener("click", () => {
   alert("Hello World");
+});
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    alert(button.id);
+  });
 });
