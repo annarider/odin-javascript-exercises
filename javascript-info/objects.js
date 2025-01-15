6576d475d46ab42ce3245bf99fcf5a77c3254dd8
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-01-15 10:30:39 -0700

    Add: Exercise 2 - Function to check for emptiness

--- javascript-info/objects.js
+++ javascript-info/objects.js
@@ -9,3 +9,15 @@ user.name = 'John';
 user.surname = 'Smith';
 user.name = 'Pete';
 delete user.name;
+
+// Write the function isEmpty(obj) which returns true 
+// if the object has no properties, false otherwise.
+
+function isEmpty(obj) {
+  return Object.keys(obj).length === 0;
+}
+
+let schedule = {};
+alert( isEmpty(schedule) ); // true
+schedule["8:30"] = "get up";
+alert( isEmpty(schedule) ); // false
