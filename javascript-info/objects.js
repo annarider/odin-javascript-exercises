c5227bd2b8d9a35d3ff33d85ea2bbf690d0ad75a
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-01-15 10:51:18 -0700

    Add: Exercise 4 - Multiply numeric property values

--- javascript-info/objects.js
+++ javascript-info/objects.js
@@ -26,9 +26,9 @@ function isEmpty(obj) {
 }
 
 let schedule = {};
-alert( isEmpty(schedule) ); // true
+console.log( isEmpty(schedule) ); // true
 schedule["8:30"] = "get up";
-alert( isEmpty(schedule) ); // false
+console.log( isEmpty(schedule) ); // false
 
 // We have an object storing salaries of our team:
 // Write the code to sum all salaries and store in the 
@@ -44,3 +44,36 @@ let sum = 0;
 for (let pay in salaries) {
   sum += salaries[pay];
 }
+
+// Create a function multiplyNumeric(obj) that multiplies 
+// all numeric property values of obj by 2.
+// Please note that multiplyNumeric does not need to return 
+// anything. It should modify the object in-place.
+// P.S. Use typeof to check for a number here.
+
+
+// before the call
+let menu = {
+  width: 200,
+  height: 300,
+  title: "My menu"
+};
+
+function multiplyNumeric(menu) {
+  for (let item in menu) {
+    if (typeof menu[item] === 'number') {
+      console.log(menu[item]);
+      menu[item] *= 2;
+    } 
+  }
+}
+
+multiplyNumeric(menu);
+
+console.log(menu);
+// after the call
+// menu = {
+//   width: 400,
+//   height: 600,
+//   title: "My menu"
+// };
