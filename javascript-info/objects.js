de11026141f040844528336699ac7a1ea8929241
Anna Rider <3461836+annarider@users.noreply.github.com>
2025-01-15 10:37:01 -0700

    Add: Exercise 3 - Sum object properties

--- javascript-info/objects.js
+++ javascript-info/objects.js
@@ -13,6 +13,14 @@ delete user.name;
 // Write the function isEmpty(obj) which returns true 
 // if the object has no properties, false otherwise.
 
+Solution: 
+// function isEmpty(obj) {
+//   for (key in obj) {
+//     return false; // if the loop has started, there is a property
+//   }
+//   return true;
+// }
+
 function isEmpty(obj) {
   return Object.keys(obj).length === 0;
 }
@@ -21,3 +29,18 @@ let schedule = {};
 alert( isEmpty(schedule) ); // true
 schedule["8:30"] = "get up";
 alert( isEmpty(schedule) ); // false
+
+// We have an object storing salaries of our team:
+// Write the code to sum all salaries and store in the 
+// variable sum. Should be 390 in the example above.
+// If salaries is empty, then the result must be 0.
+
+let salaries = {
+  John: 100,
+  Ann: 160,
+  Pete: 130
+}
+let sum = 0;
+for (let pay in salaries) {
+  sum += salaries[pay];
+}
